7b048d5f2af36c00d2cd5afe692305c08d04c60c
tzn <137900114@qq.com>
2022-08-04 13:44:24 +0800

    stb_image integration

--- src/output.rs
+++ src/output.rs
@@ -1,11 +1,13 @@
+use core::panic;
 use std::{
     error::Error,
     fs::{File, OpenOptions},
     io::Write,
-    ops::Div,
+    ops::Div, fmt::write,
 };
 
 use crate::{vec3::*, Config};
+use stb_image_write_rust::ImageWriter::ImageWriter;
 
 pub struct Output<'a> {
     file: File,
@@ -72,3 +74,51 @@ pub fn output_color_as_u64(pixel_color: &Color) -> (u64, u64, u64) {
         (255.999f64 * pixel_color.2) as u64,
     )
 }
+
+//by default our component size is 3
+pub struct ImageStream {
+    width : i32,
+    height : i32,
+    data   : Vec<u8>,
+    spp    : i32
+}
+
+impl ImageStream {
+    pub fn new(config : &Config)->ImageStream{
+        ImageStream 
+        { 
+            width: config.image_width, 
+            height: config.image_height, 
+            data: Vec::new(),
+            spp : config.samples_per_pixel
+        }
+    }
+
+    pub fn output_color(&mut self,color : &Color){
+        if self.data.len()  >= (self.width * self.height * 3) as usize {
+            panic!("ImageStream::output_color : image out of bondary");
+        }
+        let (r,g,b) = output_color_as_u8(&color.div(self.spp as f64));
+        self.data.push(r);
+        self.data.push(g);
+        self.data.push(b);
+    }
+
+    pub fn save_png(&self,path:&str){
+        if self.data.len() != (self.width * self.height * 3) as usize{
+            panic!("ImageStream::save : not enough data have written to image stream yet");
+        }
+        let mut write_path = String::from(path);
+        match path.find(".png") {
+            Some(_) => (),
+            None => {
+                write_path.push_str(".png");
+            }
+        };
+        let mut writer = ImageWriter::new(write_path.as_str());
+        writer.write_png(self.width, self.height, 3, self.data.as_ptr());
+
+    }
+}
+
+
